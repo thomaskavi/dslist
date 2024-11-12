862aa82a25b9a2a80d1e389719094c23c2b50b10
thomaskavi <thomaskavi2000@gmail.com>
2024-11-12 15:00:44 -0300

    Game seed

--- src/main/java/com/thomaskavi/dslist/entities/Game.java
+++ src/main/java/com/thomaskavi/dslist/entities/Game.java
@@ -33,7 +33,11 @@ public class Game {
   private String platforms;
   private Double score;
   private String imgUrl;
+
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
 }
